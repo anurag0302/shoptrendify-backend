1b8e62fe2a6e92f24b26b45c66e43c4c254d3837
anuragchhetri <anurag.chhetri@nagarro.com>
2023-10-10 11:22:48 +0530

    homepage

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,10 +1,15 @@
 const productsData = require("../Data.json");
+const productsData2 = require("../Data2.json");
+const productsData3 = require("../Data3.json");
+const HomeData = require("../HomeData.json");
+
+
 
 
 const fetchData = (offset, limit) => {
     const startIndex = parseInt(offset);
     const endIndex = startIndex + parseInt(limit);
-    const paginatedProducts = productsData.slice(startIndex, endIndex);
+    const paginatedProducts = productsData3.slice(startIndex, endIndex);
     return paginatedProducts;
   };
 
@@ -12,12 +17,22 @@ const getAll = async (req, res) => {
   try {
     const { offset = 0, limit = 20 } = req.query;
     const products = fetchData(offset, limit);
+    // const products =productsData3;
+
     res.status(200).json(products);
   } catch (error) {
     res.status(401).json({ message: "Token is not valid" });
   }
 };
+const getHomeData = async (req, res) => {
+    try {
+      res.status(200).json(HomeData);
+    } catch (error) {
+      res.status(401).json({ message: "Token is not valid" });
+    }
+  };
 
 module.exports = {
   getAll,
+  getHomeData,
 };
--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -3,6 +3,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/products', productsController.getAll);
+router.get('/home', productsController.getHomeData);
+
 
 
 module.exports = router;
